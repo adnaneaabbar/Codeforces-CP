98090634d2a09eab67412a3e223c5091ddae4c8c
Adnane Aabbar <44345798+parzival69@users.noreply.github.com>
2019-11-23 15:02:21 +0100

    https://codeforces.com/problemset/problem/474/B

---
+++ worms.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, const char** argv) {
+
+    int n; cin >> n;
+    int a[n];
+    cin >> a[0];
+    for(int i = 1; i < n; i++){
+        cin >> a[i];
+        a[i] += a[i-1];
+    }
+    
+    int m; cin >> m;
+    int worm;
+    for(int i = 0; i < m; i++){
+        cin >> worm;
+        for(int j = 1; j <= n; j++){
+            if(worm <= a[j-1]){
+                cout << j << endl;
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
